503646531a5fd1dec4167cfcc40ba4b79f357b49
Chinmai Raman <raman105@mail.chapman.edu>
2016-04-14 07:22:36 +0000

    final edits

--- Lagrange_poly3.py
+++ Lagrange_poly3.py
@@ -16,11 +16,17 @@ from __future__ import division
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sp
+from unittest import TestCase
 
 class LagrangeInterpolation:
 
-    def __init__(self, xp, yp):
-        self.xp, self.yp = xp, yp
+    def __init__(self, arg1, arg2, arg3 = None):
+        if isinstance(arg1, np.ndarray) and isinstance(arg2, np.ndarray):
+            self.xp, self.yp = arg1, arg2
+        elif isinstance(callable(arg1) is True) and isinstance(arg2, list) and isinstance(arg3, int):
+            self.f, self.x, self.n = arg1, arg2, arg3
+        else:
+            print "Error. Provide parameters of correct type"
 
     def L_k(self, x, k):
         """
@@ -57,10 +63,11 @@ class LagrangeInterpolation:
             summation += self.L_k(x, k) * self.yp[k]
         return summation
 
-def test_p_L():
-    xp = np.linspace(0, np.pi, 5)
-    yp = np.sin(xp)
-    p_L = LagrangeInterpolation(xp, yp)
-    x = 1.2
-    for i in xrange(len(p_L.yp)):
-        assert(abs(p_L(p_L.xp[i]) - p_L.yp[i]) < 1e-3), 'bug in class LagrangeInterpolation'
+class Test_LagrangeInterpolation(TestCase):
+    def test_p_L(self):
+        xp = np.linspace(0, np.pi, 5)
+        yp = np.sin(xp)
+        p_L = LagrangeInterpolation(xp, yp)
+        x = 1.2
+        for i in xrange(len(p_L.yp)):
+            assert(abs(p_L(p_L.xp[i]) - p_L.yp[i]) < 1e-3), 'bug in class LagrangeInterpolation'
